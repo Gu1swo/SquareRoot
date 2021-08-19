ccdc608eea72833d003b6a5c78815062b2c41b82
DESKTOP-8L5GPVH\User <igormarushenko@gmail.com>
2021-08-19 15:19:34 +0300

    Calculate square root program without using a sqrt function

---
+++ Calculate Square Root/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculate_Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, root = 1, i = 1;
+
+            Console.WriteLine("enter a number to calculate sqrt");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= input) {
+                if (root * i == input) {
+                    Console.WriteLine("the sqrt of the number: " + input + " is: " + root);
+                    break;
+                }  
+                root++;
+                i++;
+            }
+
+            if(root * root != input)
+                Console.WriteLine(input + " is not a square root");
+        }
+    }
+}
